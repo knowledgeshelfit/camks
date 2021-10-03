a6721fa1f5748c2ad75643d97dbfdb38fd46063d
Knowledge Shelf <60973040+knowledgeshelfit@users.noreply.github.com>
2021-10-03 21:59:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,48 @@
 import cv2
 import streamlit as st
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+
+from streamlit_webrtc import (
+    AudioProcessorBase,
+    RTCConfiguration,
+    VideoProcessorBase,
+    WebRtcMode,
+    webrtc_streamer,
+)
 
 
-class VideoTransformer(VideoTransformerBase):
-    def __init__(self):
-        self.threshold1 = 100
-        self.threshold2 = 200
-
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-
-        img = cv2.cvtColor(
-            cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR
-        )
-
-        return img
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
 
 
-ctx = webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+def main():
+    st.header("WebRTC demo")
+    
+    programatically_control_page = "Control the playing state programatically"
+    app_mode = st.sidebar.selectbox(
+        "Choose the app mode",
+        [
+            programatically_control_page,
+        ],
+    )
+    st.subheader(app_mode)
 
-if ctx.video_transformer:
-    ctx.video_transformer.threshold1 = st.slider("Threshold1", 0, 1000, 100)
-    ctx.video_transformer.threshold2 = st.slider("Threshold2", 0, 1000, 200)
+    
+    if app_mode == programatically_control_page:
+        app_programatically_play()
+
+
+def app_programatically_play():
+    """ A sample of controlling the playing state from Python. """
+    playing = st.checkbox("Playing", value=True)
+
+    webrtc_streamer(
+        key="media-constraints",
+        desired_playing_state=playing,
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+    )
+
+
+if __name__ == "__main__":
+    main()
